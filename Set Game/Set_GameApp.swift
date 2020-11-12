eb4ca503dc5e4c6d13f5fe55192d88c75733ef0c
Mohamed Elasfoury <mohamed.elasfoury@epfl.ch>
2020-11-12 18:46:51 +0100

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by asfoury on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
